73665827dfad52e7cf9c4ae5baf1d46d93b24510
BashirShallah <bashir.shallah@gmail.com>
2019-01-11 18:36:33 +0300

    add UsersForm Component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ViewUser from "./components/ViewUser";
 import {getUsers, deleteUser} from "./Api/Users";
+import UsersForm from "./components/UsersForm";
 
 class App extends Component {
 
@@ -56,6 +57,15 @@ class App extends Component {
           <h3>User Details</h3>
           {this.state.user.id > 0 ? 
             <ViewUser user={this.state.user} />
+            : 'please select a user'
+          }
+        </div>
+        <div>
+          <h3>Edit User</h3>
+          {this.state.user.id > 0 ? 
+            <UsersForm 
+              values={this.state.user} 
+              onSubmit={(values=>console.log(values))}/>
             : 'please select a user'
           }
         </div>
---
+++ src/components/UsersForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup";
+
+export default function UsersForm(props){
+    const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required()
+    });
+
+    return <Formik 
+        enableReinitialize={true}
+        initialValues={props.values}
+        onSubmit={props.onSubmit}
+        validationSchema={schema}
+        render={props=>{
+            return <Form>
+                <label>Name</label>
+                <Field name="name" />
+                <ErrorMessage name="name" />
+
+                <label>Email</label>
+                <Field name="email" />
+                <ErrorMessage name="email" />
+
+                <button type="submit">Send</button>
+            </Form>
+        }}
+    />
+
+}
